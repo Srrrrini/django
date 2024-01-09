12921e4cc3ba29afe0aed2e52d5257b2a5a2ecba
Srrrrini <89584032+Srrrrini@users.noreply.github.com>
2024-01-09 08:14:44 +0400

    Add files via upload

--- django-task/userdashboard/users/views.py
+++ django-task/userdashboard/users/views.py
@@ -5,15 +5,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PatientForm, DoctorForm
 
+
+
 def signup(request):
     if request.method == 'POST':
         user_type = request.POST.get('user_type')
+        
+        # Initialize form with None
+        form = None
+
         if user_type == 'patient':
             form = PatientForm(request.POST)
         elif user_type == 'doctor':
             form = DoctorForm(request.POST)
         
-        if form.is_valid():
+        # Check if form is not None and is valid
+        if form and form.is_valid():
             user = form.save()
             # Redirect to login or dashboard as per your application flow
             return redirect('login')
@@ -21,6 +28,8 @@ def signup(request):
         form = PatientForm()  # Default form
 
     return render(request, 'signup.html', {'form': form})
+
+
 
 @login_required
 def dashboard(request):
